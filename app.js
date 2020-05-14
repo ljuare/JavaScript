d0cf90e694f61f5bbb833f33df4e4b720368688e
Nigger-cyber <lucasjuarezsibello@gmail.com>
2020-05-14 20:27:59 -0300

    Simplificando JS con map() y Arrow Function

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Hola desde la terminal")
+const express = require("express")
+
+const server = express()
+
+const docs = express.static("docs")
+
+server.use(docs)
+
+server.get("/holas.html", function(request, response){ // Con ctrl + c apago la consola
+    response.end("Acá van a mostrarse las ofertas MercadoTECH")
+})
+
+//server.post()
+
+server.listen(80)
