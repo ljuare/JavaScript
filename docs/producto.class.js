65be85cba84f98eb047a0a96f15da95cc338d5a9
Nigger-cyber <lucasjuarezsibello@gmail.com>
2020-05-31 16:22:52 -0300

    Virtual DOM + posteo de datos con AJAX

--- docs/producto.class.js
+++ docs/producto.class.js
@@ -8,23 +8,63 @@ class Producto { // Lo ideal siempre es tener separada la clase
         this.precio = precio
         this.imagen = imagen
         this.marca = marca
+        this.estado = false
+        this.vDOM = document.createElement("article")
     }
 
     Mostrar(){ // Metodo de instancia: Ya que para usar este metodo debo crear el objeto
-        const ficha = document.createElement("article") // Cuando se ejecuta el createElement solo le ponemos el nombre de la etiqueta
-        ficha.classList.add("col-4")
-        ficha.innerHTML = `<div class="card h-100">
+        //const ficha = document.createElement("article") // Cuando se ejecuta el createElement solo le ponemos el nombre de la etiqueta
+        // Lo de arriba ya no es necesario debido a que ya defini el createElement arriba con el virtual DOM
+
+        this.vDOM.classList.add("col-4")
+        this.vDOM.innerHTML = `<div class="card h-100">
                             <a href="#">
                                 <img class="card-img-top img-fluid" src=${this.imagen} alt="">
                             </a>
                             <div class="card-body">
-                                <h4 class="card-title"><a href="#">${this.marca} - ${this.nombre}</a> <span class="badge badge-pill badge-success float-right">$${this.precio.toFixed(2)}</span></h4>
-                                <p class="card-textr">${this.stock} unid.</p>
-                                <button class="btn btn-primary float-right">Comprar</button>
+                                <h4 class="card-title"><a href="#">${this.marca} - ${this.nombre}</a> <span class="badge badge-pill badge-success float-right">$${parseFloat(this.precio).toFixed(2)}</span></h4>
+                                <p class="card-textr">${parseInt(this.stock)} unid.</p>
+                                <button class="btn btn-warning btn-editar float-left">Editar</button>
+                                <button class="btn btn-primary btn-comprar float-right">Comprar</button>
                             </div>
                         </div>`
+        
+        // Con esto voy a decirle que si el estado es falso entonces me haga un appendchild y sino que no me haga nada
+        if (!this.estado){ // Es como decir this.estado = false. 
+            document.querySelector("#productos-destacados").appendChild(this.vDOM)
+            this.estado = true
+        }
 
-        document.querySelector("#productos-destacados").appendChild(ficha)
+
+        this.vDOM.querySelector(".btn-editar").onclick = (evento) => { // Si yo utilizara function y no arrow el this seria el boton editar por lo que no me traeria el objeto sino el boton
+            this.marca = prompt("Ingrese nueva marca: ", this.marca)
+            this.nombre = prompt("Ingrese nuevo nombre: ", this.nombre)
+            this.stock = prompt("Ingrese nuevo stock: ", this.stock)
+            this.precio = prompt("Ingrese nuevo precio: ", this.precio)
+            this.imagen = prompt("Ingrese nueva url: ", this.imagen)
+
+            this.Mostrar() // Hasta aca lo que hice fue reutilizar informacion pero para crear un nuevo article, yo quiero que me lo reemplace
+
+            //Aca voy a enviar los nuevos datos al servidor...
+            let datos = new FormData()
+            datos.append("marca", this.marca)
+            datos.append("nombre", this.nombre)
+            datos.append("stock", this.stock)
+            datos.append("precio", this.precio)
+            datos.append("imagen", this.imagen)
+
+            let config  = {
+                method : "POST",
+                headers : {
+                    "Content-Type" : "application/x-www-form-urlencoded"
+                },
+                body : datos
+            }
+            fetch("https://webhook.site/08984cf8-0c9b-4115-bad4-da50123a865c", config)
+
+            console.log(this) // El this es el objeto producto con el que se armó la interfaz
+        }
+
 
     }
 
